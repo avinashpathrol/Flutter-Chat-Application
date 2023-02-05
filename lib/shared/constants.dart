7edc9f801b5dd4ee58a14e51291293269e672e18
AvinashPathrol <35654094+avinashpathrol@users.noreply.github.com>
2023-02-04 18:22:42 -0700

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:116623825381:web:009a15111bcc781143370c";
+  static String apiKey = "AIzaSyDPFNRR5yrqcNedKFtabyaE5Vb8Re8Oh9A";
+  static String messagingSenderId = "116623825381";
+  static String projectId = "chatappflutter-c8b02";
+  final primaryColor = const Color(0xFFee7b64);
+}
